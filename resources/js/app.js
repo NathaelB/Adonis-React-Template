adbb6c4c22ac607497b7140fd3cf07d92f5bf6ef
Nathael Bonnal <kirua48000@gmail.com>
2022-04-27 09:08:12 +0200

    :tada: init app React-Adonis

---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+import '../css/app.css'
+import React from 'react'
+import {render} from 'react-dom'
+import {createInertiaApp} from "@inertiajs/inertia-react";
+
+createInertiaApp({
+  resolve: name => import(`./pages/${name}`),
+  setup({el, App, props}) {
+    render(<App {...props} />, el)
+  }
+})
---
+++ resources/js/pages/home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home () {
+
+  return (
+    <div className=" h-screen overflow-hidden">
+      <h1 className="text-center h-full text-4xl text-gray-900 font-bold transform translate-y-[35%]">Adonis App with ReactJS</h1>
+    </div>
+  )
+}
